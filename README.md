8115238174be25fb39913f9a76c5ed92a46b4e37
“rileyrich03” <rileyrichardson0703@gmail.com>
2023-10-02 21:53:54 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # WEboard
+Describe WEboard
+# ToDo
+How to make .jar
+How to use establish remote connections -O- Java RMI
+How to make GUI
+How to listen to listen to global keypresses
+    maybe ?  https://github.com/kwhat/jnativehook
---
+++ src/WEboard.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class WEboard {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);        
+        boolean running = true;
+        while(running) {
+            String input = kb.next().toLowerCase();
+            switch(input) {
+                case "quit":
+                    running = false;
+                    break;
+                default:
+                    System.out.printf("Command %s unkown.", input);
+            }
+        }
+    }
+}
